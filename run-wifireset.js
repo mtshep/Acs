e1de1907f82393edf6dff5622611aa1efc3a72a5
mtshep <callum.shepherd@manxtelecom.com>
2025-02-13 10:23:41 +0000

    Create run-wifireset.js

---
+++ run-wifireset.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { exec } = require('child_process');
+const app = express();
+const port = 3000;
+
+app.post('/run-wifireset', (req, res) => {
+    exec('wifireset.js', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing script: ${error}`);
+            return res.status(500).send('Error executing script');
+        }
+        console.log(`Script output: ${stdout}`);
+        res.send('WiFi reset script executed successfully');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
